8372a2c6056d87d0a9c2b6d79af0a2457b58bf71
kidtzz <bangkitpr4@gmail.com>
2022-01-26 17:10:25 +0700

    kedua ges

---
+++ src/component/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="header">
+            <h1>Hello ges</h1>
+        </div>
+    );
+};
+
+export default Navbar;
