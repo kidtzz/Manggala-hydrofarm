2549998dc77275f801ef9e6837df2d35719434e1
kidtzz <bangkitpr4@gmail.com>
2022-01-27 18:02:50 +0700

    lagee

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,69 +4,75 @@ import Button from "../component/button/button";
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg sticky-top">
-            <div class="container">
-                <a class="navbar-brand" href="  ">
-                    <img
-                        className="img-fluid "
-                        width="100"
-                        height="24"
-                        src={logo}
-                        alt=""
-                    />
-                </a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon">
-                        <i className="fas fa-bars"></i>
-                    </span>
-                </button>
-                <div
-                    class="collapse navbar-collapse justify-content-end text-center"
-                    id="navbarNav"
-                >
-                    <ul class="navbar-nav align-items-center">
-                        <li class="nav-item">
-                            <a class="nav-link " aria-current="page" href="  ">
-                                Home
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="  ">
-                                About
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="  ">
-                                Services
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="  ">
-                                Product
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="  ">
-                                Contact
-                            </a>
-                        </li>
-                        <li class="nav-item fw-bold">
-                            <a class="" href=" ">
-                                <Button />
-                            </a>
-                        </li>
-                    </ul>
+        <div className="header-inner">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container">
+                    <a class="navbar-brand" href="  ">
+                        <img
+                            className="img-fluid "
+                            width="100"
+                            height="24"
+                            src={logo}
+                            alt=""
+                        />
+                    </a>
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon">
+                            <i className="fas fa-bars"></i>
+                        </span>
+                    </button>
+                    <div
+                        class="collapse navbar-collapse justify-content-end text-center"
+                        id="navbarNav"
+                    >
+                        <ul class="navbar-nav align-items-center">
+                            <li class="nav-item">
+                                <a
+                                    class="nav-link "
+                                    aria-current="page"
+                                    href="  "
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="  ">
+                                    About
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="  ">
+                                    Services
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="  ">
+                                    Product
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="  ">
+                                    Contact
+                                </a>
+                            </li>
+                            <li class="nav-item fw-bold">
+                                <a class="" href=" ">
+                                    <Button />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     );
 };
 
