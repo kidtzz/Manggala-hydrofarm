4f94aee3048cf7a7fec36b70429cac1bd2122515
kidtzz <bangkitpr4@gmail.com>
2022-02-03 13:25:17 +0700

    hh

--- src/component/Content.js
+++ src/component/Content.js
@@ -4,14 +4,15 @@ const Content = () => {
     return (
         <section className="py-5 bg-content2">
             <div className="container px-5 my-5 justify-content-center">
-                <div className="row gx-5">
-                    <div className="col-lg-4 mb-5 mb-lg-0">
+                <div className="row gx-5 justify-content-center align-items-center">
+                    <h2>Kenapa Memilih Manggala Hydrofarm?</h2>
+                    <div className="col-lg-4 col-xl-4 col-md-4 mb-5 mb-lg-0">
                         <h2>_</h2>
                     </div>
-                    <div className="col-lg-4 mb-5 mb-lg-0">
+                    <div className="col-lg-4 col-lg-4 col-xl-4 col-md-4 mb-5 mb-lg-0">
                         <h2>_</h2>
                     </div>
-                    <div className="col-lg-4 mb-5 mb-lg-0">
+                    <div className="col-lg-4 mb-5 col-lg-4 col-xl-4 col-md-4 mb-lg-0">
                         <h2>""</h2>
                     </div>
                 </div>
