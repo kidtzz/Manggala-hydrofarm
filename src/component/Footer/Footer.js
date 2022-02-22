98bd339c20caee7ac6c998694f48213c5ac66db0
kidtzz <bangkitpr4@gmail.com>
2022-02-22 22:34:14 +0700

    hehe

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -9,39 +9,61 @@ const Footer = () => {
                     <div className="col-lg-4 col-md-4 col-xl-4 mb-5 mb-lg-0">
                         <h6 className=" mb-4 fw-bold">About us</h6>
                         <p className="text-secondary">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Aliquam voluptatibus fugit sit temporibus
-                            saepe asperiores deserunt dolorem harum amet
-                            praesentium veritatis, itaque ratione, dolor illum
-                            pariatur iusto ipsam. Sequi, quia!
+                            <a href=" ">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Aliquam voluptatibus fugit sit
+                                temporibus saepe asperiores deserunt dolorem
+                                harum amet praesentium veritatis, itaque
+                                ratione, dolor illum pariatur iusto ipsam.
+                                Sequi, quia!
+                            </a>
                         </p>
                     </div>
                     <div className="col-lg-3 col-md-3 col-xl-3  mb-5 mb-lg-0">
                         <h6 className=" mb-4 fw-bold">Contact</h6>
-                        <div className="footer-link d-block text-secondary ">
-                            <p>
-                                {" "}
-                                <i class="fab fa-whatsapp "> </i> +62 8222 9617
-                                7554
-                            </p>
-                            <p>
-                                {" "}
-                                <i class="fas fa-envelope "> </i>{" "}
-                                ManggalaHydrofarm@gmail.com
-                            </p>
-                            <p>
-                                {" "}
-                                <i class="fas fa-map-marker-alt"> </i> Manggal
-                                RT 17, RW 06, Simo, Simo, Boyolali, Jawa tengah,
-                                Indonesia
-                            </p>
+                        <div className="footer-link d-block text-secondary d-flex ">
+                            <ul className="text-center ">
+                                <li>
+                                    {" "}
+                                    <i class="fab fa-whatsapp "> </i>
+                                </li>
+                                <li>
+                                    {" "}
+                                    <i class="fas fa-envelope "> </i>
+                                </li>
+                                <li>
+                                    <i class="fas fa-map-marker-alt"> </i>
+                                </li>
+                            </ul>
+                            <ul className="">
+                                <li>
+                                    <a href=" "> +62 8222 9617 7554 </a>
+                                </li>
+                                <li>
+                                    <a href=" ">ManggalaHydrofarm@gmail.com</a>
+                                </li>
+                                <li>
+                                    <a href=" ">
+                                        {"   "}
+                                        Manggal RT 17, RW 06, Simo, Simo,
+                                        Boyolali, Jawa tengah, Indonesia
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                     <div className="col-lg-2 col-md-2 col-xl-2  mb-5 mb-lg-0">
                         <h6 className="  mb-4 fw-bold">Learn & Support</h6>
                         <div className="text-secondary">
-                            <li>lorem</li>
-                            <li>lorem</li>
+                            <ul>
+                                <li>
+                                    <a href=" ">Lorem</a>{" "}
+                                </li>
+                                <li>
+                                    {" "}
+                                    <a href=" ">Lorem </a>{" "}
+                                </li>
+                            </ul>
                         </div>
                     </div>
                     <div className="col-lg-2 col-md-2 col-xl-2  mb-5 mb-lg-0">
